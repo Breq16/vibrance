47d3f1d9b1884fb827aa72b3454b310869f33fe6
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-05-20 00:23:35 -0400

    Restructure relay server into package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Breq16/vibrance",
-    packages=["vibrance"],
+    packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: MIT License",
