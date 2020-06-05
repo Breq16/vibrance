c6adf1a7a7ef83cc04196f305f386a5000ad7be9
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-06-05 17:03:55 -0400

    Controller handles errors with relay connection gracefully

--- vibrance/controller.py
+++ vibrance/controller.py
@@ -1,14 +1,16 @@
 import socket
+import selectors
 import json
 import time
 import ssl
 
+from . import tolerant_socket
 
 class Controller:
     """Manages a connection with a relay server and sends new messages."""
 
     def __init__(self):
-        pass
+        self.connected = False
 
     def connect(self, relay, psk=None, enable_ssl=True):
         """Connects to a relay server at the given address. If psk is provided,
@@ -16,32 +18,25 @@ class Controller:
         connect to the server using SSL."""
 
         if enable_ssl:
-            self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-            self.context.load_default_certs()
-            unwrapped_socket = socket.socket(socket.AF_INET,
-                                             socket.SOCK_STREAM)
-            self.socket = self.context.wrap_socket(unwrapped_socket,
-                                                   server_hostname=relay)
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+            context.load_default_certs()
         else:
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((relay, 9999))
+            context = None
 
-        if psk:
-            self.socket.send(psk.encode("utf-8"))
-            ret = self.socket.recv(1024)
-            if ret == b"OK":
-                return
-            else:
-                raise ValueError("authentication failed")
+        self.socket = tolerant_socket.TolerantSocket()
+        self.socket.connect(relay, 9999, psk, context)
 
     def write(self, messages):
         """Send messages to the relay server to be broadcasted to clients.
         Returns performance data from both the relay server and local
         measurements."""
 
+        self.socket.repair()
+
         timestamp = time.time()
         self.socket.send((json.dumps(messages)+"\n").encode("utf-8"))
+
         stats = {}
-        stats["server"] = json.loads(self.socket.recv(1024).decode("utf-8"))
+        stats["server"] = self.socket.recvJSON()
         stats["controller"] = {"latency": int((time.time()-timestamp)*1000)}
         return stats
---
+++ vibrance/tolerant_socket.py
@@ -0,0 +1,101 @@
+import socket
+import ssl
+import select
+import logging
+import json
+
+class TolerantSocket:
+    def __init__(self, debug_level=logging.CRITICAL):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(debug_level)
+        self.logger.info("Created tolerant socket")
+
+    def connect(self, host, port, psk=None, ssl_context=None):
+        self.logger.info("Set target at %s:%i", host, port)
+        self.host = host
+        self.port = port
+        self.psk = psk
+        self.context = ssl_context
+
+        self.makeSocket()
+
+    def makeSocket(self):
+        self.logger.info("Creating new socket object...")
+        unwrapped = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        unwrapped.settimeout(10)
+
+        if self.context:
+            self.socket = self.context.wrap_socket(unwrapped, server_hostname=self.host)
+        else:
+            self.socket = unwrapped
+
+        try:
+            self.socket.connect((self.host, self.port))
+        except ConnectionError as e:
+            self.logger.error("Connection failed: %s", e)
+            self.socket.close()
+            self.socket = None
+
+        if self.psk and self.socket:
+            self.logger.info("Attempting auth...")
+            self.send(self.psk.encode("utf-8"))
+            if self.socket:
+                ret = self.recv(1024)
+                if self.socket:
+                    if ret == b"OK":
+                        return
+                    else:
+                        self.socket.close()
+                        self.socket = None
+
+    def send(self, data):
+        if self.socket:
+            try:
+                self.socket.send(data)
+            except ConnectionError as e:
+                self.logger.error("Send failed: %s", e)
+                self.socket.close()
+                self.socket = None
+            else:
+                self.logger.debug("Send successful")
+        else:
+            self.logger.error("Failed to send, not connected")
+
+    def recv(self, length):
+        if self.socket:
+            try:
+                data = self.socket.recv(length)
+            except ConnectionError as e:
+                self.logger.error("Recv failed: %s", e)
+                self.socket.close()
+                self.socket = None
+            else:
+                if len(data) == 0:
+                    self.logger.error("Socket disconnected")
+                    self.socket.close()
+                    self.socket = None
+                else:
+                    self.logger.debug("Recv successful, returned %s", data)
+                    return data
+        else:
+            self.logger.error("Failed to recv, not connected")
+
+    def repair(self):
+        if not self.socket:
+            self.logger.info("Attempting repair...")
+            self.makeSocket()
+
+    def recvJSON(self, length=1024):
+        data = self.recv(length)
+        if data:
+            try:
+                data = data.decode("utf-8")
+            except UnicodeDecodeError:
+                self.logger.error("Invalid UTF-8 received")
+            else:
+                try:
+                    data = json.loads(data)
+                except json.JSONDecodeError:
+                    self.logger.error("Invalid JSON received")
+                else:
+                    return data
