145642e115f5c6aa7a0e9b529096743c255cf66c
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-06-08 20:14:05 -0400

    Pass host as argument to vibrance.client

--- vibrance/client/__main__.py
+++ vibrance/client/__main__.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask, send_file, render_template
 
 app = Flask(__name__)
@@ -10,7 +11,7 @@ def index():
 
 @app.route("/selector.html")
 def selector():
-    return render_template("selector.html", host="cloud.itsw.es")
+    return render_template("selector.html", host=sys.argv[1])
 
 
 @app.route("/app.html")
@@ -25,12 +26,12 @@ def appjs():
 
 @app.route("/viewall.html")
 def viewall():
-    return render_template("viewall.html", host="cloud.itsw.es")
+    return render_template("viewall.html", host=sys.argv[1])
 
 
 @app.route("/stress.html")
 def stress():
-    return render_template("stress.html", host="cloud.itsw.es")
+    return render_template("stress.html", host=sys.argv[1])
 
 
 if __name__ == "__main__":
